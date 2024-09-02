2075271d646ab78167f422a2f627e1b8763df00a
Michael Iyke <ymichaelc@gmail.com>
2024-09-02 23:26:50 +0100

    Task 02

---
+++ controllers/AppController.js
@@ -0,0 +1,25 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const getStatus = (req, res) => {
+  const redisAlive = redisClient.isAlive();
+  const dbAlive = dbClient.isAlive();
+
+  res.status(200).json({ redis: redisAlive, db: dbAlive });
+};
+
+const getStats = async (req, res) => {
+  try {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+
+    res.status(200).json({ users: usersCount, files: filesCount });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+module.exports = {
+  getStatus,
+  getStats,
+};
