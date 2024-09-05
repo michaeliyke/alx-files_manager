7ade32f84d2647b9bd05c4e7ee414baffb69e513
cyril <cyrildaniels21+git@gmail.com>
2024-09-05 04:45:55 +0100

    fix bug in task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,10 +1,10 @@
 import dbClient from '../utils/db';
+// import redisClient from '../utils/redis';
 
 const crypto = require('crypto');
 
 const postNew = async (req, res) => {
   const { email, password } = req.body;
-  const users = dbClient.db.collection('users');
 
   if (!email) {
     return res.status(400).json({ error: 'Missing email' });
@@ -14,6 +14,7 @@ const postNew = async (req, res) => {
     return res.status(400).json({ error: 'Missing password' });
   }
 
+  const users = dbClient.db.collection('users');
   if ((await users.findOne({ email }))) {
     return res.status(400).json({ error: 'Already exist' });
   }
@@ -26,20 +27,19 @@ const postNew = async (req, res) => {
     password: hashedPassword,
   };
 
-  users.insertOne(newUser);
-  console.log(newUser);
-  return res.status(201).json({ email: newUser.email, id: newUser._id });
+  const result = await users.insertOne(newUser);
+  return res.status(201).json({ id: result.insertedId, email: newUser.email });
 };
 
-const getMe = async (req, res) => {
-  const { token } = req.headers;
-  const users = dbClient.db.collection('users');
-  const userId = await redisClient.get(`auth_${token}`);
-  const user = await users.findOne({ userId });
-  if (!user) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-  return res.status(200).json({ email: user.email, id: user._id });
-};
+// const getMe = async (req, res) => {
+//   const { token } = req.headers;
+//   const users = dbClient.db.collection('users');
+//   const userId = await redisClient.get(`auth_${token}`);
+//   const user = await users.findOne({ userId });
+//   if (!user) {
+//     return res.status(401).json({ error: 'Unauthorized' });
+//   }
+//   return res.status(200).json({ email: user.email, id: user._id });
+// };
 
-export default { postNew, getMe };
+export default { postNew };
